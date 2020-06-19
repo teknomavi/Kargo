66fd067aca73ffd940777de79351b2fafe3f6b53
Selahaddin <selahaddinh123@gmail.com>
2020-06-19 15:25:28 +0300

    changed to loop instead of single package

--- src/Company/Yurtici/Service.php
+++ src/Company/Yurtici/Service.php
@@ -13,46 +13,50 @@ use Teknomavi\Kargo\Model\Package;
  */
 class Service extends ServiceAbstract implements ServiceInterface
 {
-    private $packageToBeSend;
     private $YKClient;
 
     function __construct($options) {
         $this->YKClient = new YKSoapClient($options);
-        $this->packageList = array();
     }
 
     public function sendPackages(){
-        $package = $this->packageToBeSend; // will be converted to for loop
-        $res = $this->YKClient->createShipment(
-            $package->getReferenceNo(),
-            $package->getInvoiceNo(),
-            $package->getConsigneeName(),    //Consigliere to Michael: 
-            $package->getConsigneeAddress(), // Even the shooting of your father
-            $package->getConsigneeCity(),    // was business, not personal. 
-            $package->getConsigneeTown(),    // You should know that by now
-            $package->getConsigneeMobilPhone(),
-            "","",
-            $package->getConsigneeEmail(),
-            "","","",
-            $package->getNumberOfPackages()
-        );
-        //print_r($res);
-        $response = new CreateShipment();
-        if($res->ShippingOrderResultVO->outFlag == 0){
-            $response = $response->setSuccess(true);
-            $response->setReferenceNumber($res->cargoKey);
+        //$package = $this->packageToBeSend; // will be converted to for loop
+        $responses = array();
+        foreach ($this->packages as $package) {
+            //print_r($package);
+            $res = $this->YKClient->createShipment(
+                $package->getReferenceNo(),
+                $package->getInvoiceNo(),
+                $package->getConsigneeName(),    //Consigliere to Michael: 
+                $package->getConsigneeAddress(), // Even the shooting of your father
+                $package->getConsigneeCity(),    // was business, not personal. 
+                $package->getConsigneeTown(),    // You should know that by now
+                $package->getConsigneeMobilPhone(),
+                "","",
+                $package->getConsigneeEmail(),
+                "","","",
+                $package->getNumberOfPackages()
+            );
+            print_r($res);
+            
+            $response = new CreateShipment();
+            if($res->ShippingOrderResultVO->outFlag == 0){
+                $response = $response->setSuccess(true);
+                $response->setReferenceNumber($res->ShippingOrderResultVO->shippingOrderDetailVO->cargoKey);
+            }
+            else{
+                $response = $response->setSuccess(false);
+                $response = $response->setErrorDescription($res->ShippingOrderResultVO->shippingOrderDetailVO->errMessage);
+                $response = $response->setErrorCode($res->ShippingOrderResultVO->shippingOrderDetailVO->errCode);
+            }
+            $responses[$package->getReferenceNo()] = $response;
         }
-        else{
-            $response = $response->setSuccess(false);
-            $response = $response->setErrorDescription($res->ShippingOrderResultVO->shippingOrderDetailVO->errMessage);
-            $response = $response->setErrorCode($res->ShippingOrderResultVO->shippingOrderDetailVO->errCode);
-        }
-        return $response;
+        return $responses;
     }
     
     public function addPackage(Package $package)
     {
-        $this->packageToBeSend = $package;
+        array_push($this->packages, $package);
     }
 
 }
--- tests/YKTest.php
+++ tests/YKTest.php
@@ -21,9 +21,25 @@ $package->setConsigneeCountry("Turkey");
 $package->setConsigneeMobilPhone("05074320494");
 $package->setConsigneePostalCode("06540");
 $package->setInvoiceNo("TESTTES");
-$package->setReferenceNo("999999691");// cargo Key I suppose
+$package->setReferenceNo("99999692");// cargo Key I suppose
 $package->setPackageType(Package::PACKAGE_TYPE_BOX);
 $package->setShipmentType(Package::SHIPMENT_TYPE_PREPAID);
 $provider->addPackage($package);
+$package2 = new Package();
+$package2->setPaymentType(Package::PAYMENT_TYPE_SHIPPER_PAY);
+$package2->setNumberOfPackages(1); // Daha sonra toplu da denenecek
+$package2->setConsigneeName("Seha Muhendislik Musavirlik AS");
+$package2->setConsigneeTown("CANKAYA");
+$package2->setConsigneeCity("ANKARA");
+$package2->setConsigneeAddress("AYTEN SOKAK 28/2 MEBUSEVLERI");
+$package2->setConsigneeEmail("selahaddinh123@gmail.com");
+$package2->setConsigneeCountry("Turkey");
+$package2->setConsigneeMobilPhone("05074320494");
+$package2->setConsigneePostalCode("06540");
+$package2->setInvoiceNo("TESTTES");
+$package2->setReferenceNo("999933139692");// cargo Key I suppose
+$package2->setPackageType(Package::PACKAGE_TYPE_BOX);
+$package2->setShipmentType(Package::SHIPMENT_TYPE_PREPAID);
+$provider->addPackage($package2);
 $res = $provider->sendPackages();
 print_r($res);
